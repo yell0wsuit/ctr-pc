22385753d7a9254999368dd4c0eabe72c6d4f1bc
Bingies24 <74263388+Bingies24@users.noreply.github.com>
2025-12-06 11:41:35 -0500

    Fixing the cursor (Part 1)
    1. Array native cursors are disposed now before clearing the array.
    2. The native cursor is clamped to 64x64.
    
    What remains to be done:
    1. Fix touch points being broken.
    2. Change the GetData and SetData to something that won't kill performance eventually.

--- CutTheRope/Game1.cs
+++ CutTheRope/Game1.cs
@@ -13,7 +13,6 @@ using CutTheRope.Framework.Media;
 using CutTheRope.Helpers;
 
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
