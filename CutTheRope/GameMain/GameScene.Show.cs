ee0ea27809ecff5bb41669a56741ec0eb511245f
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-12-12 13:28:34 +0700

    Update GameScene.Show.cs

--- CutTheRope/GameMain/GameScene.Show.cs
+++ CutTheRope/GameMain/GameScene.Show.cs
@@ -67,7 +67,6 @@ namespace CutTheRope.GameMain
             text.SetName("levelLabel");
             text.x = 15f + Canvas.xOffsetScaled;
             text.y = SCREEN_HEIGHT + 15f;
-            text2.x = -20f;
             text2.y = 40f; // the "Level" label in game
             text2.rotationCenterX -= text2.width / 2f;
             text2.scaleX = text2.scaleY = 0.7f;
