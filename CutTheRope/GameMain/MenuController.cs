9fc242853da38e55084d3e3d365759337035a447
aidrum <baukerbeus@gmail.com>
2025-12-24 09:57:01 -0300

    code changes for correct quads
    quads are different for a lot of stuff now that there's an extra candy in the middle of the textures

--- CutTheRope/GameMain/MenuController.cs
+++ CutTheRope/GameMain/MenuController.cs
@@ -201,7 +201,7 @@ namespace CutTheRope.GameMain
                 }
 
                 // Main logo
-                Image image3 = Image.Image_createWithResIDQuad(Resources.Img.MenuLogoNew, 52);
+                Image image3 = Image.Image_createWithResIDQuad(Resources.Img.MenuLogoNew, 53);
                 image3.anchor = 10;
                 image3.parentAnchor = 10;
                 image3.y = 55f;
