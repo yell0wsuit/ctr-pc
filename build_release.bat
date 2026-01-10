a036ff1e96e6c2eebf20972ba8aeeef45bdb751d
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2026-01-11 00:27:36 +0700

    remove unnecessary options for windows building command

--- build_release.bat
+++ build_release.bat
@@ -1,3 +1,3 @@
-dotnet build CutTheRope\CutTheRope.csproj -c Release -f net9.0-windows -p:TieredCompilation=false --no-self-contained -p:IncludeSourceRevisionInInformationalVersion=false -o .\CutTheRope\bin\Publish\win-x64
+dotnet build CutTheRope\CutTheRope.csproj -c Release -f net9.0-windows -o .\CutTheRope\bin\Publish\win-x64
 dotnet publish CutTheRope\CutTheRope.csproj -c Release -f net9.0 -r osx-arm64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained -p:PublishSingleFile=true -p:IncludeSourceRevisionInInformationalVersion=false -o .\CutTheRope\bin\Publish\osx-arm64
 dotnet publish CutTheRope\CutTheRope.csproj -c Release -f net9.0 -r linux-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained -p:PublishSingleFile=true -p:IncludeSourceRevisionInInformationalVersion=false -o .\CutTheRope\bin\Publish\linux-x64
