015256e53adb5b5c6a4f7b50a6a01d570d14b6a9
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2026-01-12 09:09:32 +0700

    reduce CLI flags by moving to csproj

--- build_release.bat
+++ build_release.bat
@@ -1,3 +1,3 @@
-dotnet build CutTheRope\CutTheRope.csproj -c Release -f net9.0-windows -p:IncludeSourceRevisionInInformationalVersion=false -o .\CutTheRope\bin\Publish\win-x64
-dotnet publish CutTheRope\CutTheRope.csproj -c Release -f net9.0 -r osx-arm64 -p:PublishReadyToRun=false --self-contained -p:PublishSingleFile=true -p:IncludeSourceRevisionInInformationalVersion=false -o .\CutTheRope\bin\Publish\osx-arm64
-dotnet publish CutTheRope\CutTheRope.csproj -c Release -f net9.0 -r linux-x64 -p:PublishReadyToRun=false --self-contained -p:PublishSingleFile=true -p:IncludeSourceRevisionInInformationalVersion=false -o .\CutTheRope\bin\Publish\linux-x64
+dotnet build CutTheRope\CutTheRope.csproj -c Release -f net9.0-windows -o .\CutTheRope\bin\Publish\win-x64
+dotnet publish CutTheRope\CutTheRope.csproj -c Release -f net9.0 -r osx-arm64 -o .\CutTheRope\bin\Publish\osx-arm64
+dotnet publish CutTheRope\CutTheRope.csproj -c Release -f net9.0 -r linux-x64 -o .\CutTheRope\bin\Publish\linux-x64
