19177cf5a6a54cc351b3b2f33bb70c6036a040b0
Bingies24 <74263388+Bingies24@users.noreply.github.com>
2026-01-11 19:46:18 -0500

    moved TieredCompilation setting to csproj

--- build_release.sh
+++ build_release.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-dotnet publish CutTheRope/CutTheRope.csproj -c Release -f net9.0 -r osx-arm64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained -p:PublishSingleFile=true -p:IncludeSourceRevisionInInformationalVersion=false -o ./CutTheRope/bin/Publish/osx-arm64
-dotnet publish CutTheRope/CutTheRope.csproj -c Release -f net9.0 -r linux-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained -p:PublishSingleFile=true -p:IncludeSourceRevisionInInformationalVersion=false -o ./CutTheRope/bin/Publish/linux-x64
+dotnet publish CutTheRope/CutTheRope.csproj -c Release -f net9.0 -r osx-arm64 -p:PublishReadyToRun=false --self-contained -p:PublishSingleFile=true -p:IncludeSourceRevisionInInformationalVersion=false -o ./CutTheRope/bin/Publish/osx-arm64
+dotnet publish CutTheRope/CutTheRope.csproj -c Release -f net9.0 -r linux-x64 -p:PublishReadyToRun=false --self-contained -p:PublishSingleFile=true -p:IncludeSourceRevisionInInformationalVersion=false -o ./CutTheRope/bin/Publish/linux-x64
