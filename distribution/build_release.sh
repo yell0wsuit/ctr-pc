5d6b6112b9f88a36ed14e4d67f322a428f7f1c45
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2026-01-30 20:40:23 +0700

    add windows building script

--- distribution/build_release.sh
+++ distribution/build_release.sh
@@ -3,5 +3,6 @@
 # Linux build requirements:
 #   sudo apt install libvlc-dev vlc libx11-dev
 
+dotnet publish ../CutTheRope/CutTheRope.csproj -c Release -f net9.0 -r win-x64 -o ../CutTheRope/bin/Publish/win-x64
 dotnet publish ../CutTheRope/CutTheRope.csproj -c Release -f net9.0 -r osx-arm64 -o ../CutTheRope/bin/Publish/osx-arm64
 dotnet publish ../CutTheRope/CutTheRope.csproj -c Release -f net9.0 -r linux-x64 -o ../CutTheRope/bin/Publish/linux-x64
