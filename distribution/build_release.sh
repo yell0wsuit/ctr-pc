f818508fe965afd75f896fc4ec0c588b9581a583
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2026-01-30 19:51:41 +0700

    add video dependency note when building

--- distribution/build_release.sh
+++ distribution/build_release.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# Linux build requirements:
+#   sudo apt install libvlc-dev vlc libx11-dev
+
 dotnet publish ../CutTheRope/CutTheRope.csproj -c Release -f net9.0 -r osx-arm64 -o ../CutTheRope/bin/Publish/osx-arm64
 dotnet publish ../CutTheRope/CutTheRope.csproj -c Release -f net9.0 -r linux-x64 -o ../CutTheRope/bin/Publish/linux-x64
