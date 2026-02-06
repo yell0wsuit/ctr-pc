c37ded9dec308f300d4e2aa9549352664d4f0c7d
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2026-02-06 23:18:38 +0700

    Update build_release.sh

--- distribution/build_release.sh
+++ distribution/build_release.sh
@@ -11,5 +11,5 @@ if [ -z "$VERSION" ]; then
 fi
 
 dotnet publish ../CutTheRope/CutTheRope.csproj -c Release -f net10.0 -r win-x64 -p:VersionPrefix="$VERSION" -p:VersionSuffix= -o ../CutTheRope/bin/Publish/win-x64
-dotnet publish ../CutTheRope/CutTheRope.csproj -c Release -f net10.0 -r osx-arm64 -f net10.0 -p:VersionPrefix="$VERSION" -p:VersionSuffix= -o ../CutTheRope/bin/Publish/osx-arm64
+dotnet publish ../CutTheRope/CutTheRope.csproj -c Release -f net10.0 -r osx-arm64 -p:VersionPrefix="$VERSION" -p:VersionSuffix= -o ../CutTheRope/bin/Publish/osx-arm64
 dotnet publish ../CutTheRope/CutTheRope.csproj -c Release -f net10.0 -r linux-x64 -p:VersionPrefix="$VERSION" -p:VersionSuffix= -o ../CutTheRope/bin/Publish/linux-x64
