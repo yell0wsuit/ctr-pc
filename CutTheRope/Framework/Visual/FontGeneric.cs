907c8777b15d0c55b49750435963ab6800163eac
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2026-04-05 13:26:57 +0700

    add xml docs for Font and its related classes

--- CutTheRope/Framework/Visual/FontGeneric.cs
+++ CutTheRope/Framework/Visual/FontGeneric.cs
@@ -1,7 +1,14 @@
 namespace CutTheRope.Framework.Visual
 {
+    /// <summary>
+    /// Abstract base class for bitmap fonts, providing character measurement and charmap access.
+    /// </summary>
     internal abstract class FontGeneric : FrameworkTypes
     {
+        /// <summary>
+        /// Computes the total pixel width of the given string.
+        /// </summary>
+        /// <param name="str">String to measure.</param>
         public virtual float StringWidth(string str)
         {
             float totalWidth = 0f;
@@ -16,52 +23,120 @@ namespace CutTheRope.Framework.Visual
             return totalWidth - spacing;
         }
 
+        /// <summary>
+        /// Sets the character spacing, line spacing, and space character width.
+        /// </summary>
+        /// <param name="co">Character offset (spacing between characters).</param>
+        /// <param name="lo">Line offset (spacing between lines).</param>
+        /// <param name="sw">Width of the space character.</param>
         public abstract void SetCharOffsetLineOffsetSpaceWidth(float co, float lo, float sw);
 
+        /// <summary>
+        /// Returns the font height in pixels.
+        /// </summary>
         public abstract float FontHeight();
 
+        /// <summary>
+        /// Returns true if the font can draw the specified character.
+        /// </summary>
+        /// <param name="c">Character to check.</param>
         public abstract bool CanDraw(char c);
 
+        /// <summary>
+        /// Returns the pixel width of the specified character.
+        /// </summary>
+        /// <param name="c">Character to measure.</param>
         public abstract float GetCharWidth(char c);
 
+        /// <summary>
+        /// Returns the charmap index for the specified character.
+        /// </summary>
+        /// <param name="c">Character to look up.</param>
         public abstract int GetCharmapIndex(char c);
 
+        /// <summary>
+        /// Returns the quad index for the specified character.
+        /// </summary>
+        /// <param name="c">Character to look up.</param>
         public abstract int GetCharQuad(char c);
 
+        /// <summary>
+        /// Returns the character offset (spacing) for the character at position <paramref name="c"/> in the string.
+        /// </summary>
+        /// <param name="s">Character array of the string.</param>
+        /// <param name="c">Index of the current character.</param>
+        /// <param name="len">Total length of the string.</param>
         public abstract float GetCharOffset(char[] s, int c, int len);
 
+        /// <summary>
+        /// Returns the line offset (spacing between lines).
+        /// </summary>
         public virtual float GetLineOffset()
         {
             return lineOffset;
         }
 
+        /// <summary>
+        /// Returns the top spacing above the first line.
+        /// </summary>
         public virtual float GetTopSpacing()
         {
             return topSpacing;
         }
 
+        /// <summary>
+        /// Called when a <see cref="Text"/> element is created with this font.
+        /// </summary>
+        /// <param name="st">Text element that was created.</param>
         public virtual void NotifyTextCreated(Text st)
         {
         }
 
+        /// <summary>
+        /// Called when a <see cref="Text"/> element using this font changes its content.
+        /// </summary>
+        /// <param name="st">Text element that changed.</param>
         public virtual void NotifyTextChanged(Text st)
         {
         }
 
+        /// <summary>
+        /// Called when a <see cref="Text"/> element using this font is deleted.
+        /// </summary>
+        /// <param name="st">Text element that was deleted.</param>
         public virtual void NotifyTextDeleted(Text st)
         {
         }
 
+        /// <summary>
+        /// Returns the total number of charmaps in this font.
+        /// </summary>
         public abstract int TotalCharmaps();
 
+        /// <summary>
+        /// Returns the charmap <see cref="Image"/> at the specified index.
+        /// </summary>
+        /// <param name="i">Charmap index.</param>
         public abstract Image GetCharmap(int i);
 
+        /// <summary>
+        /// Spacing between characters in pixels.
+        /// </summary>
         protected float charOffset;
 
+        /// <summary>
+        /// Spacing between lines in pixels.
+        /// </summary>
         protected float lineOffset;
 
+        /// <summary>
+        /// Width of the space character in pixels.
+        /// </summary>
         protected float spaceWidth;
 
+        /// <summary>
+        /// Extra spacing above the first line in pixels.
+        /// </summary>
         protected float topSpacing;
     }
 }
