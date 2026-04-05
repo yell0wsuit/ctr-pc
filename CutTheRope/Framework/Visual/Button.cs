f8d839c5ab8cf395836208850bf330baf2d94792
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2026-04-05 09:43:10 +0700

    add xml docs for Button

--- CutTheRope/Framework/Visual/Button.cs
+++ CutTheRope/Framework/Visual/Button.cs
@@ -3,6 +3,9 @@ using System.Xml.Linq;
 
 namespace CutTheRope.Framework.Visual
 {
+    /// <summary>
+    /// A <see cref="BaseElement"/> with up/down states, touch handling, and delegate-based press notification.
+    /// </summary>
     internal class Button : BaseElement
     {
         /// <summary>
@@ -52,6 +55,13 @@ namespace CutTheRope.Framework.Visual
             return this;
         }
 
+        /// <summary>
+        /// Expands the touch zone by the specified amounts on each side.
+        /// </summary>
+        /// <param name="l">Left expansion in pixels.</param>
+        /// <param name="r">Right expansion in pixels.</param>
+        /// <param name="t">Top expansion in pixels.</param>
+        /// <param name="b">Bottom expansion in pixels.</param>
         public virtual void SetTouchIncreaseLeftRightTopBottom(float l, float r, float t, float b)
         {
             touchLeftInc = l;
@@ -60,11 +70,21 @@ namespace CutTheRope.Framework.Visual
             touchBottomInc = b;
         }
 
+        /// <summary>
+        /// Overrides the default touch zone with a fixed rectangle.
+        /// </summary>
+        /// <param name="r">Rectangle defining the forced touch zone.</param>
         public virtual void ForceTouchRect(CTRRectangle r)
         {
             forcedTouchZone = r;
         }
 
+        /// <summary>
+        /// Tests whether the touch point is within the button's touch zone.
+        /// </summary>
+        /// <param name="tx">Touch X coordinate.</param>
+        /// <param name="ty">Touch Y coordinate.</param>
+        /// <param name="td">True for touch-down (no padding); false for move/up (adds padding).</param>
         public virtual bool IsInTouchZoneXYforTouchDown(float tx, float ty, bool td)
         {
             float touchPadding = td ? 0f : 15f;
@@ -73,6 +93,10 @@ namespace CutTheRope.Framework.Visual
                 : PointInRect(tx, ty, drawX - touchLeftInc - touchPadding, drawY - touchTopInc - touchPadding, width + (touchLeftInc + touchRightInc) + (touchPadding * 2f), height + (touchTopInc + touchBottomInc) + (touchPadding * 2f));
         }
 
+        /// <summary>
+        /// Sets the button state and toggles visibility of up/down child elements.
+        /// </summary>
+        /// <param name="s">New button state.</param>
         public virtual void SetState(BUTTON_STATE s)
         {
             state = s;
@@ -82,6 +106,7 @@ namespace CutTheRope.Framework.Visual
             child2.SetEnabled(s == BUTTON_STATE.BUTTON_DOWN);
         }
 
+        /// <inheritdoc />
         public override bool OnTouchDownXY(float tx, float ty)
         {
             _ = base.OnTouchDownXY(tx, ty);
@@ -93,6 +118,7 @@ namespace CutTheRope.Framework.Visual
             return false;
         }
 
+        /// <inheritdoc />
         public override bool OnTouchUpXY(float tx, float ty)
         {
             _ = base.OnTouchUpXY(tx, ty);
@@ -108,6 +134,7 @@ namespace CutTheRope.Framework.Visual
             return false;
         }
 
+        /// <inheritdoc />
         public override bool OnTouchMoveXY(float tx, float ty)
         {
             _ = base.OnTouchMoveXY(tx, ty);
@@ -122,6 +149,7 @@ namespace CutTheRope.Framework.Visual
             return false;
         }
 
+        /// <inheritdoc />
         public override int AddChildwithID(BaseElement c, int i)
         {
             int childId = base.AddChildwithID(c, i);
@@ -135,11 +163,18 @@ namespace CutTheRope.Framework.Visual
             return childId;
         }
 
+        /// <summary>
+        /// Creates a <see cref="BaseElement"/> from an XML definition. Not implemented in this class.
+        /// </summary>
+        /// <param name="xml">XML element to create from.</param>
         public virtual BaseElement CreateFromXML(XElement xml)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Extra padding in pixels added to the touch zone on move and up events.
+        /// </summary>
         public const float TOUCH_MOVE_AND_UP_ZONE_INCREASE = 15f;
 
         /// <summary>
@@ -147,23 +182,53 @@ namespace CutTheRope.Framework.Visual
         /// </summary>
         public ButtonId buttonID;
 
+        /// <summary>
+        /// Current press state of the button.
+        /// </summary>
         public BUTTON_STATE state;
 
+        /// <summary>
+        /// Delegate notified when the button is pressed.
+        /// </summary>
         public IButtonDelegation delegateButtonDelegate;
 
+        /// <summary>
+        /// Touch zone expansion on the left side in pixels.
+        /// </summary>
         public float touchLeftInc;
 
+        /// <summary>
+        /// Touch zone expansion on the right side in pixels.
+        /// </summary>
         public float touchRightInc;
 
+        /// <summary>
+        /// Touch zone expansion on the top side in pixels.
+        /// </summary>
         public float touchTopInc;
 
+        /// <summary>
+        /// Touch zone expansion on the bottom side in pixels.
+        /// </summary>
         public float touchBottomInc;
 
+        /// <summary>
+        /// Forced touch zone rectangle, or (-1,-1,-1,-1) to use the default.
+        /// </summary>
         public CTRRectangle forcedTouchZone;
 
+        /// <summary>
+        /// Represents the press state of a button.
+        /// </summary>
         public enum BUTTON_STATE
         {
+            /// <summary>
+            /// Button is in the unpressed state.
+            /// </summary>
             BUTTON_UP,
+            /// <summary>
+            /// Button is in the pressed state.
+            /// </summary>
             BUTTON_DOWN
         }
     }
