2d5c31f1d0cb22d39f171bdc533c7a1432a2b113
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2026-04-06 23:36:53 +0700

    remove leftover `pack` param in LevelResourceScanner

--- CutTheRope/GameMain/LevelResourceScanner.cs
+++ CutTheRope/GameMain/LevelResourceScanner.cs
@@ -19,7 +19,6 @@ namespace CutTheRope.GameMain
         /// Computes the gameplay resources required to instantiate a single parsed map.
         /// </summary>
         /// <param name="map">The parsed level XML.</param>
-        /// <param name="pack">The active pack index for pack-specific resources.</param>
         /// <returns>A de-duplicated array of resource identifiers needed for the map.</returns>
         public static string[] GetRequiredResources(XElement map)
         {
@@ -229,7 +228,6 @@ namespace CutTheRope.GameMain
         /// Adds Om Nom animation resources, including the pack-specific support sprite.
         /// </summary>
         /// <param name="resources">The destination set being accumulated.</param>
-        /// <param name="pack">The active pack index.</param>
         private static void AddTargetResources(HashSet<string> resources)
         {
             int skinIndex = OmNomSkinRegistry.GetSelectedSkinIndex();
