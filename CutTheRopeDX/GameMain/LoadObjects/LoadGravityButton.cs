aaecfb3963ab34d83effad1b7604d7e3cd3f18bb
Bingies24 <74263388+Bingies24@users.noreply.github.com>
2026-06-08 07:55:32 -0400

    use better-er disable logic

--- CutTheRopeDX/GameMain/LoadObjects/LoadGravityButton.cs
+++ CutTheRopeDX/GameMain/LoadObjects/LoadGravityButton.cs
@@ -18,16 +18,17 @@ namespace CutTheRopeDX.GameMain
         /// <param name="mapOffsetY">The additional map Y offset applied during loading.</param>
         private void LoadGravityButton(XElement xmlNode, float scale, float offsetX, float offsetY, int mapOffsetX, int mapOffsetY)
         {
-            if (globalGravityY != 0f)
+            if (globalGravityY == 0f)
             {
-                gravityButton = CreateGravityButtonWithDelegate(this);
-                gravityButton.visible = false;
-                gravityButton.touchable = false;
-                _ = AddChild(gravityButton);
-                gravityButton.x = (ParseIntOrZero(xmlNode.Attribute("x")?.Value) * scale) + offsetX + mapOffsetX;
-                gravityButton.y = (ParseIntOrZero(xmlNode.Attribute("y")?.Value) * scale) + offsetY + mapOffsetY;
-                gravityButton.anchor = 18;
+                return;
             }
+            gravityButton = CreateGravityButtonWithDelegate(this);
+            gravityButton.visible = false;
+            gravityButton.touchable = false;
+            _ = AddChild(gravityButton);
+            gravityButton.x = (ParseIntOrZero(xmlNode.Attribute("x")?.Value) * scale) + offsetX + mapOffsetX;
+            gravityButton.y = (ParseIntOrZero(xmlNode.Attribute("y")?.Value) * scale) + offsetY + mapOffsetY;
+            gravityButton.anchor = 18;
         }
     }
 }
