d1be4e879bbcb38b2d5a83e5ef45ee9da3ca7eff
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2026-06-14 14:58:15 +0700

    test: add xunit test project

---
+++ CutTheRopeDX.Tests/SmokeTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace CutTheRopeDX.Tests
+{
+    public class SmokeTest
+    {
+        [Fact]
+        public void TestProjectRuns()
+        {
+            Assert.True(true);
+        }
+    }
+}
