21fc3cfc542a65e2ea2a5af09035130d7c02e59e
Jinwen XU <80377812+Jinwen-XU@users.noreply.github.com>
2026-07-21 00:53:16 +1000

    fix: correct abbreviation

--- CutTheRopeDX/Framework/LanguageHelper.cs
+++ CutTheRopeDX/Framework/LanguageHelper.cs
@@ -227,7 +227,7 @@ namespace CutTheRopeDX.Framework
                 "ko" => "Korean",
                 "ja" => "Japanese",
                 "zh" => "Chinese",
-                "zh_tw" => "Chinese (Trad.)",
+                "zh_tw" => "Trad. Chinese",
                 _ => code,
             };
         }
